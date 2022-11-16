526c85ca0f48eb35782a6151039e90ff6fe4e96a
Owen Plesko <owenplesko@gmail.com>
2022-11-16 14:35:03 -0500

    first terminal commit

---
+++ components/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div className='searchbar'>
+        <input type='text' className='input' placeholder='summoner name...'/>
+        <button className='btn'>
+            search
+        </button>
+	</div>
+  )
+}
+
+export default Search
