42a6ec305ee2158a32e89f356c9d937c27ec016e
Owen Plesko <owenplesko@gmail.com>
2022-11-22 15:07:19 -0500

    worked onf profile header design

--- components/profile/Header.js
+++ components/profile/Header.js
@@ -5,13 +5,13 @@ import SummonerIcon from './SummonerIcon'
 
 const ProfileHeader = ({name, profileIconId, summonerLevel, rankedData}) => {
   return (
-    <div className={styles.header}>
+    <div className={styles.header}> 
       <div>
-        <label className={styles.name}>{name}</label>
-        <SummonerIcon className={styles.summonericon} profileIconId={profileIconId} summonerLevel={summonerLevel} />
+        <div className={styles.namewrapper}>
+          <label className={styles.name}>{name}</label>
+        </div>
+        <SummonerIcon profileIconId={profileIconId} summonerLevel={summonerLevel} />
       </div>
-      <RankCard queueName={"Ranked TFT"} league={rankedData.rankedTft} />
-      <RankCard queueName={"Ranked TFT"} league={rankedData.rankedTft} />
       <RankCard queueName={"Ranked TFT"} league={rankedData.rankedTft} />
     </div>
   )
--- components/profile/SummonerIcon.js
+++ components/profile/SummonerIcon.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import styles from '../../styles/profile/SummonerIcon.module.css'
+import Image from 'next/image'
 
-const SummonerIcon = ({className, profileIconId, summonerLevel}) => {
+const SummonerIcon = ({profileIconId, summonerLevel}) => {
   return (
-    <div className={`${className} ${styles.wrapper}`}>
-      <img className={styles.icon} src={`https://ddragon.leagueoflegends.com/cdn/12.22.1/img/profileicon/${profileIconId}.png`}/>
+    <div className={styles.wrapper}>
+      <Image 
+        className={styles.image}
+        src={`https://ddragon.leagueoflegends.com/cdn/12.22.1/img/profileicon/${profileIconId}.png`}
+        layout='fill'
+        objectFit='contain'
+      />
       <label className={styles.level} >{summonerLevel}</label>
     </div>
   )
